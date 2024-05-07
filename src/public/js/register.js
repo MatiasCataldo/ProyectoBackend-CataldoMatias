fa97056092e2c58f2f9f4e147fb384c675962e1a
Matias Cataldo <maticataldo21@gmail.com>
2024-05-07 18:24:10 -0300

    Update register.js

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', e => {
     const data = new FormData(form);
     const obj = {}
     data.forEach((value, key) => obj[key] = value)
-    fetch('/api/jwt/register', {
+    fetch('https://proyectobackend-cataldomatias-production.up.railway.app/api/jwt/register', {
         method: 'POST',
         body: JSON.stringify(obj),
         headers: {
