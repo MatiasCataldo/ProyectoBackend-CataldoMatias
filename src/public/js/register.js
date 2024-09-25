0f24433f94033c7543b37eb3f4beb004870ca6bf
Matias Cataldo <maticataldo21@gmail.com>
2024-09-25 12:39:00 -0300

    Update register.js

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', e => {
     const data = new FormData(form);
     const obj = {}
     data.forEach((value, key) => obj[key] = value)
-    fetch('http://localhost:8080/api/jwt/register', {
+    fetch('/api/jwt/register', {
         method: 'POST',
         body: JSON.stringify(obj),
         headers: {
